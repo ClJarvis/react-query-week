eb13f3c98b178a7df279a07044e7d44d2ce5be3f
Chris <clj@ollure.com>
2021-04-01 18:53:30 -0500

    added query client

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import { useQuery } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 import './App.css';
+/// adding to test git updates
 
 const fetchUsers = async () => {
-	try {
-	  return await (await fetch('https://reqres.in/api/users')).json();
-	} catch(err) {
-		throw new Error(err);
+	const response = await fetch('https://reqres.in/api/users');
+	if(!response.ok){
+		throw new Error('Something went wrong!');
 	}
+	return response.json();
+};
+
+const addUser = async user => {
+	const response = await fetch('https://reqres.in/api/users', {
+		method: "POST",
+		body:JSON.stringify({
+			first_name: user.first_name,
+			last_name: user.last_name
+		}),
+		headers:{
+			'Content-type': 'application/json; charset=UTF-8'
+		}
+	});
+	if(!response.ok){
+		throw new Error('Something went wrong!');
+	}
+	return response.json(); 
 }
 
-
 function App() {
-	// Grab all users
-	const { data : users, isloading, error } = useQuery('users', fetchUsers);
+	// call the userQueryClient
+	const queryClient = useQueryClient();
 
-	if (isloading) return <p>Loading ...</p>;
-	if (error) return <p>Something went wrong ...</p>;
+	// Grab all users
+  const { data: users, isLoading, error } = useQuery(
+    'users',
+    fetchUsers
+  );
+	// create mutaution to add user
+	const { mutate, mutateAsync, isLoading: isAddingUser, error: addError } = useMutation(addUser);
+	
+
+	const handleAddUser = async () => {
+		const newUser = await mutateAsync({first_name: 'React Query', last_name: 'Rules!'});
+		console.log("this was an async mutation");
+		console.log(newUser);
+	//	queryClient.invalidateQueries('users');
+		queryClient.setQueryData('users', oldData => ({
+			...oldData, 
+			data: [newUser, ...oldData.data]
+		}));
+		
+	};
+
+	if (isLoading) return <p>Loading</p>;
+	if ( error || addError) return <p>Something went wrong ...</p>;
 
 	console.log(users);
 
 	  return (
-    <div className="App">
+	  	<div className='App'>
+    	{isAddingUser ? <p>Adding user ..</p> : null}
+    <button onClick={handleAddUser}>Add User</button>
       {users.data.map(user => (
       	<p key={user.id}> 
       		{user.first_name} {user.last_name}
